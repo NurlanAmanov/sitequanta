e2002c19526646d682afe3f84a5b43b53dfe840f
unknown <nurlanemenov14@gmail.com>
2024-09-01 18:46:38 +0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+    document.getElementById("mySidebar").classList.add("active");
+    document.querySelector(".main").classList.add("active");
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").classList.remove("active");
+    document.querySelector(".main").classList.remove("active");
+}
